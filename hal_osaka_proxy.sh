40609e13448a7ede6140728e870b7cd0daa861f0
tikasan <satak47cpc@gmail.com>
2016-11-09 20:57:22 +0900

    add: 環境変数に対応

--- hal_osaka_proxy.sh
+++ hal_osaka_proxy.sh
@@ -4,6 +4,11 @@ git config --global --unset http.proxy
 git config --global --unset https.proxy
 git config --global --unset url."https://".insteadOf
 
+unset http_proxy
+unset https_proxy
+unset ftp_proxy
+unset all_proxy
+
 # HAL
 if ping -c 1 proxy03.osaka.hal.ac.jp >/dev/null 2>&1
 then
@@ -11,4 +16,9 @@ then
     git config --global http.proxy $proxy_hal
     git config --global https.proxy $proxy_hal
     git config --global url."https://".insteadOf git://
+
+    export http_proxy=$proxy_hal
+    export https_proxy=$proxy_hal
+    export ftp_proxy=$proxy_hal
+    export all_proxy=$proxy_hal
 fi
