84101616d4437d1bb9115dc8ded60ccb77a2d46c
tikasan <satak47cpc@gmail.com>
2016-11-09 21:28:40 +0900

    add: 変数で認証情報を入力できるようにした。

--- hal_osaka_proxy.sh
+++ hal_osaka_proxy.sh
@@ -2,6 +2,11 @@
 # (c) 2016 jumpei"pei"chikamori http://ganbaruman.xyz/photofolio
 
 #!/bin/bash
+
+# 設定してください
+user=学籍番号
+password=認証PW
+
 # 初期設定
 git config --global --unset http.proxy
 git config --global --unset https.proxy
@@ -15,7 +20,7 @@ unset all_proxy
 # HAL
 if ping -c 3 proxy03.osaka.hal.ac.jp >/dev/null 2>&1
 then
-    proxy_hal=http://学籍番号:認証PW@proxy03.osaka.hal.ac.jp:8080
+    proxy_hal=http://$user:$password@proxy03.osaka.hal.ac.jp:8080
     git config --global http.proxy $proxy_hal
     git config --global https.proxy $proxy_hal
     git config --global url."https://".insteadOf git://
